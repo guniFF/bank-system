e6ccc509f6eb13c1b08848013767034d10bb8ed1
giniFF <sky4tony@naver.com>
2026-08-14 17:52:37 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/test/java/com/guniff/banksystem/BanksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guniff.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
