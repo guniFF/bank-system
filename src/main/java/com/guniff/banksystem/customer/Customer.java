7e0b7b33a9aa057b7260d299d0590b3ee1a2c3ac
giniFF <sky4tony@naver.com>
2026-08-17 21:42:12 +0900

    feat : Customer Entity 생성

--- src/main/java/com/guniff/banksystem/customer/Customer.java
+++ src/main/java/com/guniff/banksystem/customer/Customer.java
@@ -5,17 +5,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Customer {
 
     @Id
-    @GeneratedValue // 추후 시퀀스 설정
+    @GeneratedValue
     private Long customerId;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     @NotBlank
     private String name;
 
     protected Customer() {}
+
 }
